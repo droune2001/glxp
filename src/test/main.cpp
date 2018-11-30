80a8795725b836f23a65cd23b2176050e0fd5106
droune2001 <nfauvet@gmail.com>
2018-11-30 17:25:16 +0100

    Added Imgui

--- src/test/main.cpp
+++ src/test/main.cpp
@@ -1,10 +1,12 @@
-#include <GL/glew.h>
-#include <GLFW/glfw3.h>
-#include "glm_usage.h"
+#include "imgui.h"
+#include "imgui_impl_glfw.h"
+#include "imgui_impl_opengl3.h"
 
 #include <stdlib.h>
 #include <stdio.h>
-
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include "glm_usage.h"
 
 static
 void error_callback(int error, const char* description)
@@ -38,6 +40,7 @@ int main(int argc, char **argv)
     
     glfwSetErrorCallback(error_callback);
 
+    const char* glsl_version = "#version 460";
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
     GLFWwindow* window = glfwCreateWindow(640, 480, "My Title", NULL, NULL);
@@ -52,6 +55,7 @@ int main(int argc, char **argv)
     //glfwSetWindowCloseCallback
 
     glfwMakeContextCurrent(window);
+    glfwSwapInterval(1);
     if (glewInit() != GLEW_OK)
     {
         glfwDestroyWindow(window);
@@ -59,19 +63,52 @@ int main(int argc, char **argv)
         return EXIT_FAILURE;
     }
 
-    glfwSwapInterval(1);
+    // Setup Dear ImGui binding
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO(); (void)io;
+    //io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;  // Enable Keyboard Controls
+    //io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;   // Enable Gamepad Controls
+
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
+    ImGui_ImplOpenGL3_Init(glsl_version);
+
+    // Setup style
+    ImGui::StyleColorsDark();
+    //ImGui::StyleColorsClassic();
 
     // INIT GL RESOURCES
 
+    bool show_demo_window = true;
+    bool show_another_window = false;
+    ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+
     while (!glfwWindowShouldClose(window))
     {
+        glfwPollEvents();
+
+        // Start the Dear ImGui frame
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
+
+        // 1. Show the big demo window (Most of the sample code is in ImGui::ShowDemoWindow()! You can browse its code to learn more about Dear ImGui!).
+        if (show_demo_window)
+            ImGui::ShowDemoWindow(&show_demo_window);
+
+        ImGui::Render();
+
         // Keep running
         run(window);
 
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
         glfwSwapBuffers(window);
-        glfwPollEvents();
     }
 
+    // Cleanup
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext();
     glfwDestroyWindow(window);
     glfwTerminate();
     return EXIT_SUCCESS;
